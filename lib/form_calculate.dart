858d97258b4b133e1cd0cecee4fbd66398ff1cee
Aji Suryawan <kazumovie132@gmail.com>
2021-08-19 15:17:03 +0700

    Initial commit

---
+++ lib/form_calculate.dart
@@ -0,0 +1,129 @@
+import 'package:flutter/material.dart';
+class FormCalculate extends StatefulWidget {
+  const FormCalculate({Key? key}) : super(key: key);
+
+  @override
+  _FormCalculateState createState() => _FormCalculateState();
+}
+
+class _FormCalculateState extends State<FormCalculate> {
+
+  TextEditingController inputcontrollerExpectedRetirement= new TextEditingController();
+  TextEditingController inputcontrollerRiskProfile= new TextEditingController();
+  TextEditingController inputcontrollerExpectedInvestement= new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    int currentage=55;
+    return Scaffold(
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Form Validation Example"),
+              Text("current age "+currentage.toString()),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: inputcontrollerExpectedRetirement,
+                decoration: InputDecoration(
+                  hintText: "input number"
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                controller: inputcontrollerRiskProfile,
+                decoration: InputDecoration(
+                  hintText: "risk profile"
+                ),
+              ),
+              SizedBox(height: 20,),
+              TextFormField(
+                onChanged: (value) {
+                  if(inputcontrollerRiskProfile.text == ""){
+                    inputcontrollerExpectedInvestement.text="";
+                    showAlertDialogRiskProfile(context);
+                  }
+                },
+                keyboardType: TextInputType.number,
+                controller: inputcontrollerExpectedInvestement,
+                decoration: InputDecoration(
+                  hintText: "expected investment"
+                ),
+              ),
+              ElevatedButton(onPressed: () {
+                calculateData(inputcontrollerExpectedRetirement.text,currentage);
+              }, child: Text("Calculate"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void calculateData(String data,int currentage) {
+    if(int.parse(data)>= currentage && int.parse(data)<=100){
+      print("bener nih proses data");
+    }else{
+      showAlertDialogExpectedRetirement(context);
+    }
+  }
+
+  showAlertDialogRiskProfile(BuildContext context) {
+
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("My title"),
+      content: Text("Silahkan mengisi risk profile terlebih dahulu"),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+
+  showAlertDialogExpectedRetirement(BuildContext context) {
+
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.pop(context);
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("My title"),
+      content: Text("Expected Retirement tidak boleh kurang dari current age dan maksimal 100"),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
+}
